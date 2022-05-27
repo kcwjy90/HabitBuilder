3002696664efbe88013edd6bfbfc9367368aebcd
kcwjy90 <wondlek@hotmail.com>
2022-05-27 11:58:33 -0700

    RepeatVC
    RepeatVC - added collapsable cells so user can see repeat options when they tap on sections

--- HabitBuilder/HabitBuilder/VCs/Main/RepeatVC.swift
+++ HabitBuilder/HabitBuilder/VCs/Main/RepeatVC.swift
@@ -15,6 +15,21 @@ protocol RepeatVCDelegate: AnyObject {
     //    func didCreateNewHabit(title: String, desc: String, date: Date, time: Date)
 }
 
+class Section {
+    let title: String
+    let options: [String]
+    var isOpened: Bool = false
+    
+    init(title: String,
+         options: [String],
+         isOpened: Bool = false
+    ) {
+        self.title = title
+        self.options = options
+        self.isOpened = isOpened
+    }
+}
+
 class RepeatVC: UIViewController {
     
     weak var delegate: RepeatVCDelegate?   // Delegate property var 생성
@@ -68,8 +83,17 @@ class RepeatVC: UIViewController {
         return v
     }()
     
+    private var sections = [Section]()
+        
     override func loadView() {
         super.loadView()
+        
+        sections = [
+            Section(title: "Daily", options:["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].compactMap({return "\($0)"})),
+            Section(title: "Weekly", options:["Every Week","Every 2 Weeks","Every 3 Weeks","Every 4 Weeks","Every 5 Weeks"].compactMap({return " \($0)"})),
+            Section(title: "Monthly", options:["1st of Each Month","5th of Each Month","10th of Each Month","15th of Each Month","20th of Each Month","25th of Each Month","Last day of Each Month"].compactMap({return "\($0)"})),
+            Section(title: "Custom", options:["Once Every __ Days"].compactMap({return "\($0)"}))
+        ]
         
         view.addSubview(backView)
         backView.addSubview(backButton)
@@ -106,10 +130,9 @@ class RepeatVC: UIViewController {
             make.height.equalTo(40)
         }
         
-        
         // repeatTableView grid
         repeatTableView.snp.makeConstraints { (make) in
-            make.top.equalTo(pageLabel.snp.bottom)
+            make.top.equalTo(pageLabel.snp.bottom).offset(10)
             make.left.right.bottom.equalTo(backView)
         }
         
@@ -120,13 +143,32 @@ class RepeatVC: UIViewController {
 
 //Adding tableview and content
 extension RepeatVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        let section = sections[section]
+        
+        if section.isOpened {
+            return section.options.count + 1
+        } else {
+            return 1
+        }
     }
 
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-                print("Row: \(indexPath.row)")
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.row == 0 { //이렇게 하지 않으면 cell을 tap한 순간 collapse 된다.
+            sections[indexPath.section].isOpened = !sections[indexPath.section].isOpened
+            tableView.reloadSections([indexPath.section], with: .none)
+        } else {
+            print(indexPath.row)
+        }
+        
     }
 
 
@@ -140,8 +182,15 @@ extension RepeatVC: UITableViewDelegate, UITableViewDataSource {
         else {
             return UITableViewCell()
         }
-
+        
+        if indexPath.row == 0 {
+            cell.cellTitle.text = sections[indexPath.section].title
+        } else {
+            cell.cellTitle.text = sections[indexPath.section].options[indexPath.row - 1]
+        }
+        
         return cell
     }
+    
 
 }
